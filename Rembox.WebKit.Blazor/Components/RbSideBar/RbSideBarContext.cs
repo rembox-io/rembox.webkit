fc15e066c91fa7cc382dd5dc9e2b288ef103c65b
sshulik <sergey.shulik@gmail.com>
2021-11-05 12:50:39 +0300

    WIP: RbSideBar: expand child menu

--- Rembox.WebKit.Blazor/Components/RbSideBar/RbSideBarContext.cs
+++ Rembox.WebKit.Blazor/Components/RbSideBar/RbSideBarContext.cs
@@ -1,7 +1,22 @@
+using System;
+using Rembox.WebKit.Blazor.Core;
+
 namespace Rembox.WebKit.Blazor.Components
 {
-    public class RbSideBarContext
+    public class RbSideBarContext: ContextBase
     {
+        private string expandedItem;
         public bool IsExpanded { get; set; }
+
+        public string ExpandedItem
+        {
+            get => expandedItem;
+            set
+            {
+                Console.WriteLine($"expandedItem changed from {expandedItem} to {value}");
+                expandedItem = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
--- Rembox.WebKit.Blazor/Components/RbSideBar/RbSideBarItem.razor.cs
+++ Rembox.WebKit.Blazor/Components/RbSideBar/RbSideBarItem.razor.cs
@@ -10,9 +10,28 @@ namespace Rembox.WebKit.Blazor.Components
         
         [Parameter] public string? Icon { get; set; } = String.Empty;
         
-        [Parameter] public bool IsRoot { get; set; } = true;
+        [Parameter] public RenderFragment? ChildContent { get; set; }
 
-        [CascadingParameter]
-        public RbSideBarContext Context { get; set; } = new();
+        [Parameter] public string NavigateUrl { get; set; } = string.Empty;
+
+        public bool IsExpanded { get; set; }
+        
+        [CascadingParameter] public RbSideBarContext? Context { get; set; }
+
+        private void OnItemClicked()
+        {
+            Console.WriteLine($"item clicked {NavigateUrl}");
+            if (Context != null)
+                Context.ExpandedItem = NavigateUrl;
+        }
+
+        protected override void OnInitialized()
+        {
+            Context!.PropertyChanged += (_, _) =>
+            {
+                IsExpanded = Context.ExpandedItem == NavigateUrl;
+                StateHasChanged();
+            };
+        }
     }
 }
